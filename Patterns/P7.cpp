3c673838f4077eb78d68a4c19108378a89c5fb0e
surbhi sharma <142304352+surbhisharma004@users.noreply.github.com>
2025-09-16 01:29:48 +0530

    P7.cpp

---
+++ Patterns/P7.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter number of rows : " << endl;
+    cin >> n;
+
+  for(int i=0;i<n;i++){
+      for(int j=0;j<(n-i-1); j++)
+      cout<<" ";
+      for(int j=0; j<(2*i+1);j++)
+      cout<<"*";
+      cout<<endl;
+  }
+  return 0;
+}
